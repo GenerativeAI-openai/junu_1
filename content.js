284d1c3ba596d8528e798672f5129db348c4394b
GenerativeAI-openai <ijunu3343@gmail.com>
2025-03-14 22:54:52 +0900

    Update content.js

--- content.js
+++ content.js
@@ -89,14 +89,14 @@ async function preview() {
     // console.log(notification)
     let index = 0;
     notification.forEach(notification => {
-        if (notifications[index].template == "template_13"){//엔트리 이야기 또는 노팁, 묻답 등 커뮤니티
+        if (notifications[index].template == "template_13"){//엔트리 이야기 또는 노팁, 묻답 등 커뮤니티과 관련된 내용
             // notification.innerHTML = `<li style="cursor: default;"><div class="css-1fwo2t5 exgmeid5"><div class="css-1l656pp exgmeid3"><em class="css-uigoc8 exgmeid0">커뮤니티</em><em>${notifications[index].params[0]}</em>님이 <em>${notifications[index].params[1]}</em>에 댓글을 남겼습니다.</div><span class="css-u980j1 exgmeid1">0 분 전</span></div></li>`
             notification.innerHTML = `<li style="cursor: default;"><div class="css-1fwo2t5 exgmeid5"><div class="css-1l656pp exgmeid3"><em>${notifications[index].params[0]}</em>님이 <em>${notifications[index].params[1]}</em>에 댓글을 남겼습니다.</div><span class="css-u980j1 exgmeid1">0 분 전</span></div></li>`
-        } else if (notifications[index].template == "template_28") {//새로운 작품
+        } else if (notifications[index].template == "template_28") {//새로운 작품과 관련된 내용
             notification.innerHTML = `<li style="cursor: pointer;"><div class="css-1fwo2t5 exgmeid5"><div class="css-1l656pp exgmeid3"><em class="css-bbbvqf exgmeid0">팔로잉</em><em>${notifications[index].params[0]}</em>님이 새로운 작품을 공유했습니다.</div><span class="css-u980j1 exgmeid1">2 분 전</span></div></li>`
-        } else if (notifications[index].template == "template_27") {//팔로잉
+        } else if (notifications[index].template == "template_27") {//팔로잉과 관련된 내용
             notification.innerHTML = `<li style="cursor: pointer;"><div class="css-1fwo2t5 exgmeid5"><div class="css-1l656pp exgmeid3"><em class="css-bbbvqf exgmeid0">팔로잉</em><em>${notifications[index].params[0]}</em>님이 나를 팔로잉 합니다.</div><span class="css-u980j1 exgmeid1">49 분 전</span></div></li>`
-        } else if (notifications[index].template == "template_3") {//좋아요
+        } else if (notifications[index].template == "template_3") {//좋아요와 관련된 내용
             notification.innerHTML = `<li style="cursor: pointer;"><div class="css-1fwo2t5 exgmeid5"><span class="css-vwf6be exgmeid4" style="background-image: url(&quot;https://playentry.org${notifications[index].thumbUrl[0]}&quot;);">&nbsp;</span><div class="css-1l656pp exgmeid3"><em class="css-azelyu exgmeid0">작품</em><em>${notifications[index].params[0]}</em>님이 나의 작품을 좋아합니다.</div><span class="css-u980j1 exgmeid1">3 분 전</span></div></li>`
         }
         index++
